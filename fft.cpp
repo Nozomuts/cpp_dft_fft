0bd125f42a75a434cb09ae322b73e02ef319feb0
NozomuTsuruta <tuno.0806@icloud.com>
2021-03-13 19:48:15 +0900

    fftある程度理解したが、セグフォ

--- fft.cpp
+++ fft.cpp
@@ -1,127 +1,48 @@
-#include <algorithm> // swap
 #include <complex>
-#include <iostream>
+#include <math.h>
+#include <stdio.h>
 
-using namespace std;
+#define N 80            // 分割数
+#define Fs (double)8000 // サンプリング周波数
+#define A (double)1     // 振幅
+#define F0 (double)440  // 周波数
 
-typedef complex<double> Complex;
-typedef unsigned int uint;
+using namespace std; // cout, endl, string, to_string
 
-static Complex one(1.0, 0.0);
-static Complex ione(0.0, 1.0);
+void fft(std::complex<double> *x, int _N) {
+    // Nが1以下で早期リターン
+    if (_N <= 1) {
+        return;
+    }
 
-class FFT {
-  private:
-    Complex *array;
-    size_t size; // arrayのサイズ
-    size_t use;  // コンストラクタで与えたサイズ
+    // 偶奇に分割
+    std::complex<double> even[_N / 2]; // 偶数
+    std::complex<double> odd[_N / 2];  // 奇数
+    for (int i = 0; i < _N / 2; i++) {
+        even[i] = x[i * 2];
+        odd[i] = x[i * 2 + 1];
+    }
 
-    bool verbose; // 配列ダンプのモード指定に使う
-    size_t nextPow2(size_t s);
-    void bitReverse();
+    // 再帰
+    fft(even, _N / 2);
+    fft(odd, _N / 2);
 
-  public:
-    FFT(size_t s) : use(s), size(nextPow2(s)), verbose(false) { array = new Complex[size]; };
-    ~FFT() { delete[] array; };
-
-    Complex &operator[](int index) const {
-        if (index < 0)
-            return array[use - index];
-        return array[index];
-    };
-
-    bool setVerbose(bool b) { return verbose = b; };
-    void dump();
-
-    void fft(bool isReverse = false);
-};
-
-size_t FFT::nextPow2(size_t s)
-// s以上の最小の2のべき乗を返す
-{
-    size_t n = 1;
-    while (n < s)
-        n <<= 1;
-    return n;
-}
-
-void FFT::bitReverse() {
-    uint k, b, a;
-    for (uint i = 0; i < size; i++) {
-        k = 0;
-        b = size >> 1;
-        a = 1;
-        while (b >= a) {
-            if (b & i)
-                k |= a;
-            if (a & i)
-                k |= b;
-            b >>= 1;
-            a <<= 1;
-        }
-        if (i < k)
-            swap(array[i], array[k]);
+    // DFT計算
+    for (int k = 0; k < _N / 2; k++) {
+        std::complex<double> t = exp(std::complex<double>(0, -2 * M_PI * k / N)) * odd[k];
+        x[k] = even[k] + t;
+        x[N / 2 + k] = even[k] - t;
     }
 }
 
-void FFT::dump() {
-    uint end = verbose ? size : use;
-    for (uint i = 0; i < end; i++)
-        cout << array[i] << " ";
-    cout << endl;
-}
-
-void FFT::fft(bool isReverse) {
-    bitReverse();
-    size_t m = 2;
-    Complex w, ww, t;
-
-    while (m <= size) {
-        double arg = -2.0 * M_PI / m;
-        w = Complex(cos(arg), sin(arg));
-        if (isReverse)
-            w = one / w; //-1乗 -(-2.0*PI/size) = 2.0*PI/size
-
-        for (uint i = 0; i < size; i += m) {
-            ww = 1.0;
-            for (uint j = 0; j < m / 2; j++) {
-                int a = i + j;
-                int b = i + j + m / 2;
-
-                t = ww * array[b];
-
-                array[b] = array[a] - t;
-                array[a] = array[a] + t;
-
-                ww *= w;
-            }
-        }
-        m *= 2;
-    }
-}
-
-#define N (8)
-#define RND ((double)rand() / (double)RAND_MAX)
 int main() {
-    srand(time(NULL));
-    FFT box(N);
+    std::complex<double> x_out[N]; // 元データ
 
-    for (int i = 0; i < N; i++)
-        box[i] = Complex(RND, RND);
-    box.setVerbose(true);
-
-    Complex before[N], transform[N], itransform[N];
-    // fft前
-    for (int i = 0; i < N; i++)
-        before[i] = box[i];
-    // fft後
-    box.fft();
-    for (int i = 0; i < N; i++)
-        transform[i] = box[i];
-    cout << "before\t\t\ttransform" << endl;
+    // 元データ作成
     for (int i = 0; i < N; i++) {
-        printf("(%9lf, %9lf) -> (%9lf, %9lf)\n", before[i].real(), before[i].imag(), transform[i].real(),
-               transform[i].imag());
+        x_out[i] = std::complex<double>(A * sin(2 * M_PI * F0 * i / Fs), 0.0); // t = i / Fs
     }
+
+    fft(x_out, N);
     return 0;
 }
