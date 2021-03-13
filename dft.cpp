8074f8639e9f4b30340b1bafc994eda976570424
NozomuTsuruta <tuno.0806@icloud.com>
2021-03-14 00:30:12 +0900

    dftでもcomplex使用

--- dft.cpp
+++ dft.cpp
@@ -1,6 +1,5 @@
 #include <complex>
 #include <iostream> // for cout
-#include <math.h>   // for sin(), cos()
 
 #define N 80            // 分割数
 #define Fs (double)8000 // サンプリング周波数
@@ -9,38 +8,31 @@
 
 using namespace std; // cout, endl, string, to_string
 
-void dft(std::complex<double> x[], int _N) {
-    double SRC_re[N]; // 元データの実部
-    double SRC_im[N]; // 元データの虚部
-    double DFT_re[N]; // DFTの実部
-    double DFT_im[N]; // DFTの虚部
+void dft(complex<double> x[], int _N) {
+    complex<double> dft_out[N];
+
+    // 出力用の配列初期化
+    for (int i = 0; i < N; i++) {
+        dft_out[i] = complex<double>(0, 0);
+    }
 
     // DFT計算
     for (int k = 0; k < N; k++) {
-        DFT_re[k] = 0.0;
-        DFT_im[k] = 0.0;
         for (int n = 0; n < N; n++) {
-            DFT_re[k] += SRC_re[n] * (cos((2 * M_PI / N) * k * n)) + SRC_im[n] * (sin((2 * M_PI / N) * k * n));
-            DFT_im[k] += SRC_re[n] * (-sin((2 * M_PI / N) * k * n)) + SRC_im[n] * (cos((2 * M_PI / N) * k * n));
+            dft_out[k] += x[n] * exp(complex<double>(0.0, -2 * M_PI * k * n / N)); // (実部,虚部)
         }
-        cout << k / Fs << " " << (Fs / N) * k << " " << SRC_re[k] << " " << SRC_im[k] << " " << DFT_re[k] << " "
-             << DFT_im[k] << endl;
+        cout << dft_out[k] << endl;
     }
 }
 
-// 離散フーリエ変換 f(t) = A * sin(2 * pi * F0 * t) ( 0 <= t < 0.01 )
+// 離散フーリエ変換
 int main() {
-    double DFT_re[N]; // DFTの実部
-    double DFT_im[N]; // DFTの虚部
-
-    // ヘッダ出力 ( 時間, 角周波数, 元データ(実部), 元データ(虚部), DFT(実部), DFT(虚部) )
-    printf("t, f, x_re, x_im, X_re, X_im\n");
-
-    std::complex<double> x_out[N]; // 元データ
+    complex<double> x_out[N]; // 元データ
 
     // 元データ作成
     for (int i = 0; i < N; i++) {
-        x_out[i] = std::complex<double>(A * sin(2 * M_PI * F0 * i / Fs), 0.0); // t = i / Fs
+        // x(t) = A * sin(2 * pi * F0 * t) ( 0 <= t < 0.01 )
+        x_out[i] = complex<double>(A * sin(2 * M_PI * F0 * i / Fs), 0.0); // t = i / Fs
     }
     dft(x_out, N);
 
