b4b6db453eb1f232a3ed64e22e9b91710926e71f
NozomuTsuruta <tuno.0806@icloud.com>
2021-03-14 15:47:31 +0900

    matlabファイル追加、初期位相追加

--- dft.cpp
+++ dft.cpp
@@ -5,6 +5,7 @@
 #define Fs (double)8000 // サンプリング周波数
 #define A (double)1     // 振幅
 #define F0 (double)440  // 周波数
+#define phi (double)0   // 初期位相
 
 using namespace std; // cout, endl, string, to_string
 
@@ -31,8 +32,8 @@ int main() {
 
     // 元データ作成
     for (int i = 0; i < N; i++) {
-        // x(t) = A * sin(2 * pi * F0 * t) ( 0 <= t < 0.01 )
-        x_out[i] = complex<double>(A * sin(2 * M_PI * F0 * i / Fs), 0); // t = i / Fs
+        // x(t) = A * sin(2 * pi * F0 * t + phi) ( 0 <= t < 0.008 )
+        x_out[i] = complex<double>(A * sin(2 * M_PI * F0 * i / Fs + phi), 0); // t = i / Fs
     }
     dft(x_out, N);
 
---
+++ dft.m
@@ -0,0 +1,15 @@
+clear all
+
+Fs=8000;
+A=1;
+F0=440;
+phi=0;
+len=Fs*0.008;
+
+t=(0:len-1)/Fs;
+f=(0:len-1)*(Fs/len);
+
+x=A*sin(2*pi*F0*t+phi);
+y=fft(x);
+
+y;
