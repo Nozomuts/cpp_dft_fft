16d6ca2cbe29064448be07c35517aae992d4379a
NozomuTsuruta <tuno.0806@icloud.com>
2021-03-29 19:18:54 +0900

    fftとdftを一つにまとめる

--- main.cpp
+++ main.cpp
@@ -12,6 +12,23 @@
 using namespace std;    // cout, endl, swap
 using namespace chrono; // system_clock, duration_cast, microseconds, ofstream
 
+void dft(complex<double> x[], int _N) {
+    complex<double> dft_out[N];
+
+    // 出力用の配列初期化
+    for (int i = 0; i < N; i++) {
+        dft_out[i] = complex<double>(0, 0);
+    }
+
+    // DFT計算
+    for (int k = 0; k < N; k++) {
+        for (int n = 0; n < N; n++) {
+            dft_out[k] += x[n] * exp(complex<double>(0, -2 * M_PI * k * n / N)); // (実部,虚部)
+        }
+        cout << dft_out[k] << endl;
+    }
+}
+
 void fft(complex<double> *x, int _N, int q) {
     if (_N > 1) {
         for (int k = 0; k < _N / 2; k++) {
@@ -47,19 +64,34 @@ int main() {
         x_out[i] = complex<double>(A * sin(2 * M_PI * F0 * i / Fs + phi), 0); // t = i / Fs
     }
 
+    cout << "- - - DFT - - -" << endl;
     start = system_clock::now(); // 計測スタート時刻を保存
+    dft(x_out, N);
+    end = system_clock::now(); // 計測終了時刻を保存
+    // 要した時間を計算
+    double dft_time = static_cast<double>(duration_cast<microseconds>(end - start).count() / 1000.0);
+
+    cout << "- - - FFT - - -" << endl;
+    start = system_clock::now();
     fft(x_out, N, 0);
     bit_reverse(x_out, N);
-    end = system_clock::now(); // 計測終了時刻を保存
-    double time = static_cast<double>(duration_cast<microseconds>(end - start).count() / 1000.0); // 要した時間を計算
-    // 要した時間をミリ秒（1/1000秒）に変換して表示
+    end = system_clock::now();
+    double fft_time = static_cast<double>(duration_cast<microseconds>(end - start).count() / 1000.0);
+
     for (int i = 0; i < N; i++) {
         cout << x_out[i] << endl;
     }
-    cout << time << " ms \n";
-    ofstream ofs("dft.txt");
-    ofs << time;
-    ofs.close();
+
+    // 要した時間をミリ秒（1/1000秒）に変換して表示
+    cout << "DFT: " << dft_time << " ms" << endl;
+    cout << "FFT: " << fft_time << " ms" << endl;
+
+    ofstream dft_ofs("dft.txt");
+    dft_ofs << dft_time;
+    dft_ofs.close();
+    ofstream fft_ofs("fft.txt");
+    fft_ofs << fft_time;
+    fft_ofs.close();
 
     return 0;
 }
