ce4679152f24beeb06f73f3ee14becd1058db949
NozomuTsuruta <tuno.0806@icloud.com>
2021-04-26 07:33:47 +0900

    sinをテーブルから求める関数作成

--- sin.cpp
+++ sin.cpp
@@ -2,15 +2,35 @@
 #include <fstream>
 #include <iostream> // for cout
 
-#define N 64            // 分割数
+#define N 64 // 分割数
 
-using namespace std;    // cout, endl, swap, ios, complex
+using namespace std; // cout, endl, swap, ios, complex
+
+double Sin(int i) {
+    int n = i % N;
+    double sin_table[] = {
+        0,       0.0980171, 0.19509,  0.290285, 0.382683, 0.471397, 0.55557,  0.634393, 0.707107,
+        0.77301, 0.83147,   0.881921, 0.92388,  0.95694,  0.980785, 0.995185, 1,
+    };
+    if (n <= N / 4) {
+        return sin_table[n];
+    } else if (n <= N / 2) {
+        return sin_table[N / 2 - n];
+    } else if (n <= 3 * N / 4) {
+        return -sin_table[n - N / 2];
+    } else if (n <= N) {
+        return -sin_table[N - n];
+    } else {
+        printf("Error!");
+        return -1;
+    }
+}
 
 int main() {
 
     ofstream sin_ofs("sin.csv");
-    for (int i = 0; i < 2*N; i++) {
-        sin_ofs << sin(2 * M_PI / N * i) << "," << endl;
+    for (int i = 0; i < N; i++) {
+        sin_ofs << Sin(i) << "," << endl;
     }
     sin_ofs.close();
 
